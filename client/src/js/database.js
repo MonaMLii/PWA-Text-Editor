3147a2146c6ece4b071cd9322a3ee277ff008178
chunkybooboo <89316044+chunkybooboo@users.noreply.github.com>
2023-05-11 20:55:08 -0700

    fixed getDB

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 // Export a function we will use to PUT to the database.
-export const putDb = async (content)  => {
+export const putDb = async (content) => {
 
   // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
@@ -26,7 +26,7 @@ export const putDb = async (content)  => {
   const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.put({ content: content });
+  const request = store.put({ id: 1, value: content });
 
   // Get confirmation of the request.
   const result = await request;
@@ -48,12 +48,12 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result.value;
 };
 
 // Start the database.
